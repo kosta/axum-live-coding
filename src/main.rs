38079255eb69091081f12359b7ac9fd423059de0
Kosta Welke <konstantin.welke@understand.ai>
2022-05-10 10:42:16 +0200

    init tracing-subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use anyhow::Result;
 use axum::{routing::get, Router};
+use tracing::info;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    //initialize tracing
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    println!("Listing on http://localhost:3000");
+    info!("Listing on http://localhost:3000");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
